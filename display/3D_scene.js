bb474c05e02d1b24f9d60d9a4d20f08fb5b7b72a
Mark Desktop <marklouiscastle@gmail.com>
2023-12-31 14:10:51 -0600

    made index.html show examples

---
+++ display/3D_scene.js
@@ -0,0 +1,46 @@
+let floor = new Block(50, 100, 60, 100, 10, 100, CG_DARK_GREEN); 
+
+let trunk = new Block(50, 90, 80, 10, 50, 10, CG_BROWN);
+
+let x = 50;
+let y = 55;
+let z = 80;
+let w = 40;
+let h = 40;
+let d = 40;
+let main_leaves = new Block(x, y, z, w, h, d, CG_GREEN);
+let above = new Block(x, y - h/2, z, 20, 10, 20, CG_GREEN); //top
+
+changeLightSource(0, 1, .6);
+
+let treeRot= 60;
+treeRot= treeRot* Math.PI / 180;
+treeRot= treeRot% (2 * Math.PI);
+
+let zEye = -50;
+function draw(){
+    fillCanvas(CG_LIGHT_BLUE);
+
+    treeRot+= 0.01;
+    if(treeRot> 2 * Math.PI)
+        treeRot= 0;
+
+    floor.rotX = treeRot;
+    floor.draw();
+
+    trunk.rotX = treeRot;
+    trunk.draw();
+
+    above.rotX = treeRot;
+    above.draw();
+
+    main_leaves.rotX = treeRot;
+    main_leaves.draw();
+
+    //zEye += 0.1;
+    //console.log(zEye);
+    //changeEye(50, 50, zEye);
+}
+
+const canvas_3D_scene  = document.getElementById('canvas_3D_scene');
+const cg_canvas_3D_scene = addCGCanvas(canvas_3D_scene, draw, 100, 100);
---
+++ display/many_3D_cubes.js
@@ -0,0 +1,56 @@
+let cubes = [];
+
+for(let i = 0; i < 5; i++) {
+    for(let j = 0; j < 5; j++) {
+        for(let k = 0; k < 5; k++) {
+            let cube = new Cube(i*20 + 8, j*20 + 8, -k*20 + 110, 10, '#FF0000');
+            let cubeInfo = {
+                cube : cube,
+                colorStart : 25*i+5*j+k
+            }
+            cubes.push(cubeInfo);
+        }
+    }
+}
+
+cubes.sort((a, b) => b.cube.z - a.cube.z);
+
+let rot = 1;
+rot = rot * Math.PI / 180;
+rot = rot % (2 * Math.PI);
+
+let r = 0;
+let rRate = 1;
+let g = 0;
+let gRate = 3;
+let b = 0;
+let bRate = 5;
+
+let colorRot = 0;
+function draw() {
+    fillCanvas('#AAAAAA');
+    
+    //rot += 0.01;
+    // if(rot > 2 * Math.PI)
+    //     rot = 0;
+
+    colorRot += 0.01;
+    colorRot = colorRot % (2 * Math.PI);
+
+
+    for(let i = 0; i < cubes.length; i++) {
+        r = Math.abs(Math.sin(colorRot + cubes[i].colorStart) * 255);
+        r = Math.round(r);
+        b = Math.abs(Math.cos(colorRot + cubes[i].colorStart) * 255);
+        b = Math.round(b);
+        g = Math.abs(Math.sin(colorRot + cubes[i].colorStart + Math.PI/4) * 255);
+        g = Math.round(g);
+        cubes[i].cube.color = toColor(r, g, b);
+
+        cubes[i].cube.rotate(rot, rot, rot);
+        cubes[i].cube.draw();
+    }
+}
+
+const canvas_many_3D_cubes = document.getElementById('canvas_many_3D_cubes');
+const cg_canvas_many_3D_cubes = addCGCanvas(canvas_many_3D_cubes, draw, 100, 100);
